9b6819f2a1ff3a2574045251bceb496870aeb3c8
Courtney <CourtAlexBrown@gmail.com>
2021-09-30 13:12:52 -0400

    counter needs tweaking. multiple presses of start timer creates numerous interactions, stop timer does not stop all intervals created

--- main.js
+++ main.js
@@ -19,39 +19,92 @@
   stopTimer(): stop the timer, then render()
 */
 
-const countView = document.querySelector("#count")
-let count = 0
 
-function render () {
-  countView.replaceChildren(count)
-  console.log(count)
+
+
+
+const counter = {
+  count: 0,
+  countView: document.querySelector("#count"),
+
+  render: function render () {
+    this.countView.replaceChildren(this.count)
+    console.log(this.count)
+  },
+
+  next: function next () {
+    this.count += 1
+    this.render()
+  },
+
+  prev: function prev () {
+    this.count -= 1
+    this.render()
+  },
+  
+  reset: function reset () {
+    this.count = 0
+    this.render()
+  },
+  
+  skipForward: function skipForward () {
+    this.count += 10
+    this.render()
+  },
+  
+  skipBackward: function skipBackward () {
+    this.count -= 10
+    this.render()
+  },
+
+  startTimer: function startTimer () {
+    this.timerId = window.setInterval(() => this.next(), 1000)
+  },
+
+  stopTimer: function stopTimer () {
+    window.clearInterval(this.timerId)
+  },
 }
 
-function next () {
-  count += 1
-  render()
-}
 
-function prev () {
-  count -= 1
-  render()
-}
+const newsletterCounter = {
+  count: 500,
+  countView: document.querySelector("#newsletter-count"),
 
-function reset () {
-  count = 0
-  render()
-}
-
-function skipForward () {
-  count += 10
-  render()
-}
-
-function skipBackward () {
-  count -= 10
-  render()
+  render: counter.render,
+  next: counter.next,
+  prev: counter.prev,
+  reset: counter.reset,
+  skipForward: counter.skipForward,
+  skipBackward: counter.skipBackward,
 }
 
 
+document
+.querySelector("#controls")
+.addEventListener("click",event => {
+  const clickedElement = event.target
 
+  switch (clickedElement.id) {
+    case "next": counter.next()
+    break
 
+    case "prev": counter.prev()
+    break
+
+    case "reset": counter.reset()
+    break
+
+    case "skip-forward": counter.skipForward()
+    break
+
+    case "skip-backward": counter.skipBackward()
+    break
+
+    case "start-timer": counter.startTimer()
+    break
+
+    case "stop-timer": counter.stopTimer()
+    break
+  }
+})
